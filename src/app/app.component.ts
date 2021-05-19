d976fd9f376e3f75404cc5d701a1f0d7c20188dd
ivan_200sr <ivan_2003sr@hotmail.com>
2021-05-19 14:17:39 -0300

    app-Empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+  empleados:Empleado[]=[
+    new Empleado("Iván", "Aguilar", "Especialista", 100000),
+    new Empleado("Valentín", "Aguilar", "Técnico", 50000),
+    new Empleado("Evelyn", "Raynal", "Secretaria", 90000),
+    new Empleado("Ailén", "Aguilar", "Cajera", 25000),
+    
+
+  ];
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido,this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 }
